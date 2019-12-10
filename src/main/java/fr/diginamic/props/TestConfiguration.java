0e576c2d178d1bc4a0181ae9be7493784e4b6356
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2019-12-10 13:55:56 +0100

    tp1 jdbc

--- src/main/java/fr/diginamic/props/TestConfiguration.java
+++ src/main/java/fr/diginamic/props/TestConfiguration.java
@@ -9,18 +9,20 @@ public class TestConfiguration {
 
 	public static void main(String[] args) {
 		ResourceBundle monFichierConf = ResourceBundle.getBundle("monFichier");
-		//String driverName = monFichierConf.getString("driver");
 		
-		//System.out.println(driverName);
+//		String driverName = monFichierConf.getString("driver");
+//		
+//		System.out.println(driverName);
 //		Enumeration<String> enums = monFichierConf.getKeys();
 //		while (enums.hasMoreElements()) {
 //			String enu = enums.nextElement();
 //			  String valeur = monFichierConf.getString(enu);
 //			  System.out.println(valeur);
 //		}
+		
 		Set<String> set = monFichierConf.keySet();
 		for (String a:set){
-			System.out.println(monFichierConf.getString(a));
+			System.out.println(a+" = "+monFichierConf.getString(a));
 		}
 	
 	}
---
+++ src/main/java/fr/diginamic/props/TestConnexionJdbc.java
@@ -0,0 +1,46 @@
+package fr.diginamic.props;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import org.mariadb.jdbc.Driver;
+
+public class TestConnexionJdbc {
+
+	public static void main(String[] args) {
+
+		 try {
+		 DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+		 } catch (SQLException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		
+		 try {
+		 Connection maConnection=DriverManager.getConnection("jdbc:mariadb://localhost:3306/compta","root",null);
+		 maConnection.clearWarnings();
+		 } catch (SQLException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		 
+		 
+//		ResourceBundle result = ResourceBundle.getBundle("monFichier");
+//		try {
+//			Class.forName(result.getString("driver"));
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		try {
+//			Connection connnexion = DriverManager.getConnection(result.getString("url"), result.getString("user"), result.getString("password"));
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+
+}
