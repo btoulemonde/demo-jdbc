5cccf46b6c6685c5fa128be1985d6332f7df3ecd
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2019-12-10 11:02:51 +0100

    tp1 bis

--- src/main/java/fr/diginamic/props/TestConfiguration.java
+++ src/main/java/fr/diginamic/props/TestConfiguration.java
@@ -1,7 +1,9 @@
 package fr.diginamic.props;
 
 import java.util.Enumeration;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class TestConfiguration {
 
@@ -10,12 +12,17 @@ public class TestConfiguration {
 		//String driverName = monFichierConf.getString("driver");
 		
 		//System.out.println(driverName);
-		Enumeration<String> enums = monFichierConf.getKeys();
-		while (enums.hasMoreElements()) {
-			String enu = enums.nextElement();
-			  String valeur = monFichierConf.getString(enu);
-			  System.out.println(valeur);
+//		Enumeration<String> enums = monFichierConf.getKeys();
+//		while (enums.hasMoreElements()) {
+//			String enu = enums.nextElement();
+//			  String valeur = monFichierConf.getString(enu);
+//			  System.out.println(valeur);
+//		}
+		Set<String> set = monFichierConf.keySet();
+		for (String a:set){
+			System.out.println(monFichierConf.getString(a));
 		}
+	
 	}
 
 }
